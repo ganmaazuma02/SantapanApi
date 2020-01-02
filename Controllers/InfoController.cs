27da7ae1df605b206772e224b8d3e00e53e48e63
anas_amir_nur_rashid <anas_amir_nur_rashid@dell.com>
2020-01-02 19:50:03 +0800

    Add project files.

---
+++ Controllers/InfoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using SantapanApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SantapanApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        private readonly ApiInfo _apiInfo;
+
+        public InfoController(IOptions<ApiInfo> apiInfoWrapper)
+        {
+            _apiInfo = apiInfoWrapper.Value;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public ActionResult<ApiInfo> GetInfo()
+        {
+            return _apiInfo;
+        }
+    }
+}
---
+++ Models/ApiInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SantapanApi.Models
+{
+    public class ApiInfo
+    {
+        public string ApiName { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public string AuthorEmail { get; set; }
+
+        public string Description { get; set; }
+    }
+}
